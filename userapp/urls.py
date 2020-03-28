ee3846407521763c69dbead877d85ea49e87a3b3
Sonika <acharyasonica@gmail.com>
2020-03-28 14:31:12 +0545

    My code before merge

--- userapp/urls.py
+++ userapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib import admin
 
-from .views import loginview
+from .views import loginview,registerview
 urlpatterns=[
-    path('login/',loginview, name='login')
+    path('login/',loginview, name='login'),
+    path('register/',registerview,name='register')
 ]
 
 
--- userapp/views.py
+++ userapp/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth.models import User
+from .models import UserModel
 def loginview(request):
     return render(request,'userapp/login.html')
+
+def registerview(request):
+    if request.method=="POST":
+        fname =request.POST.get("Firstname")
+        lname =request.POST.get("Lastname")
+        username =request.POST.get("Username")
+        emailaddress =request.POST.get("Email address")
+        password=request.POST.get("Password")
+        confirmpassword=request.POST.get("Confirm Password")
+        status=request.POST.get("Status")
+        profilepicture=request.POST.get("Profile Picture")
+
+        user=User(username=username, password=password, first_name=fname, last_name=lname, email=emailaddress, is_active=True)
+        user.save()
+        usermodel=UserModel(auth=user, status=status, profilepicture=profilepicture)
+        usermodel.save()
+        return redirect('index')
+    else:
+        return render(request,'userapp/register.html')
 
 
